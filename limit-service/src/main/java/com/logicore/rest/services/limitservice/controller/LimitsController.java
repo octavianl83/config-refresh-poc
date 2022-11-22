d923fc1fe6fdd60a4725f89916c1e224e57513e0
lilao <lila.octavian@hoe.com>
2022-11-22 18:22:23 +0200

    add 2 more beans that gets configuration from different files: limits-service-db and limits-service-cfg. url is /limits-db and /limits-cfg

--- limit-service/src/main/java/com/logicore/rest/services/limitservice/controller/LimitsController.java
+++ limit-service/src/main/java/com/logicore/rest/services/limitservice/controller/LimitsController.java
@@ -1,7 +1,11 @@
 package com.logicore.rest.services.limitservice.controller;
 
 import com.logicore.rest.services.limitservice.bean.Limits;
+import com.logicore.rest.services.limitservice.bean.LimitsCfg;
+import com.logicore.rest.services.limitservice.bean.LimitsDb;
 import com.logicore.rest.services.limitservice.configuration.Configuration;
+import com.logicore.rest.services.limitservice.configuration.ConfigurationCfg;
+import com.logicore.rest.services.limitservice.configuration.ConfigurationDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +16,28 @@ public class LimitsController {
     @Autowired
     private Configuration configuration;
 
+    @Autowired
+    private ConfigurationDb configurationDb;
+
+    @Autowired
+    private ConfigurationCfg configurationCfg;
+
     @GetMapping("/limits")
     public Limits retreiveLimits() {
         return new Limits(configuration.getMinimum(), configuration.getMaximum(),
                 configuration.getAverage(), configuration.getEnv());
 
+    }
+
+    @GetMapping("/limits-db")
+    public LimitsDb retreiveLimitsDb() {
+        return new LimitsDb(configurationDb.getUsername(), configurationDb.getPassword(),
+                configurationDb.getIp(), configurationDb.getPort(), configurationDb.getEnv());
+    }
+
+    @GetMapping("/limits-cfg")
+    public LimitsCfg retreiveLimitsCfg() {
+        return new LimitsCfg(configurationCfg.getRefresh(), configurationCfg.getTimeout(),
+                configurationCfg.getPersistence(), configurationCfg.getEnv());
     }
 }
